83d312729a3afca4cfa1a8e7400752394a3805f2
EfranDev <efren.saenz@hotmail.com>
2017-04-29 21:07:33 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+TWRP device tree for the ALCATEL POP 2 (ALTO45)
+========================================================
+
+For building TWRP only.
+
+Thanks Efran
---
+++ omni_alto45.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/alcatel/alto45/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := alto45
+PRODUCT_NAME := omni_alto45
+PRODUCT_BRAND := alcatel
+PRODUCT_MANUFACTURER := Alcatel
+PRODUCT_MODEL := POP2
