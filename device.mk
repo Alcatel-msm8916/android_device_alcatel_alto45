4a44dbaba569a28bc2efb940e682a22b69f4a0a9
EfranDev <efren.saenz@hotmail.com>
2017-05-01 13:30:52 -0500

    Added healthd prebuilt

--- device.mk
+++ device.mk
@@ -2,6 +2,7 @@ OCAL_PATH := device/alcatel/alto45
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dt.img:dt.img \
+    $(LOCAL_PATH)/recovery/sbin/healthd \
     $(LOCAL_PATH)/recovery/init.recovery.qcom.rc:root/init.recovery.qcom.rc
 
 $(call inherit-product, build/target/product/full.mk)
