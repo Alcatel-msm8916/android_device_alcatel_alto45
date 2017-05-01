8e66245a88f30a534debe2c0f0ba8f54a1c064fd
EfranDev <efren.saenz@hotmail.com>
2017-05-01 13:12:24 -0500

    Typo

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -3,5 +3,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 # Hack for build
-$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libminui_intermediates)
-$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libminui_intermediates/export_includes)
+$(shell mkdir -p $(OUT)/obj/STATIC_LIBRARIES/libminui_intermediates)
+$(shell touch $(OUT)/obj/STATIC_LIBRARIES/libminui_intermediates/export_includes)
